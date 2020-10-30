a011e0f196c8c2e1af4809331199e8f0cff37b25
Bodia Valeur <bodiavaleur@Bodias-MacBook-Air.local>
2020-10-30 03:31:41 +0200

    Refactor details

--- src/components/molecules/Details/Details.jsx
+++ src/components/molecules/Details/Details.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Title, Text } from "../../atoms";
 import { DetailsUI } from "./DetailsUI";
+import { GiRoundStar } from "react-icons/gi";
 
-export function Details({ title, movieYear, rating, description }) {
+export function Details({ details }) {
+  // Some properties may differ in TV and movie data
+  const title = details.title || details.original_name;
+  let releaseDate = details.release_date || details.first_air_date;
+  releaseDate = releaseDate.split("-")[0];
+
   return (
     <DetailsUI>
-      <Title>
-        {title} ({movieYear})
-      </Title>
-      <Text>rate {rating}</Text>
-      <Text>{description}</Text>
+      <Title>{title}</Title>
+      <Text>{releaseDate}</Text>
+      <Text>
+        <GiRoundStar /> {details.vote_average} ({details.vote_count})
+      </Text>
+      <Text>{details.overview}</Text>
     </DetailsUI>
   );
 }
--- src/components/molecules/Details/DetailsUI.js
+++ src/components/molecules/Details/DetailsUI.js
@@ -1,16 +1,11 @@
 import styled from "styled-components";
-import { breakpoints } from "../../../ui/breakpoints";
 
 export const DetailsUI = styled.div`
+  width: 100%;
+  min-height: 20rem;
   display: flex;
-  max-width: 40rem;
   flex-flow: column nowrap;
   justify-content: space-evenly;
+  align-items: center;
   padding: 1rem;
-  border-radius: 0.5rem;
-  background: #333;
-
-  @media screen and (${breakpoints.tablet}) {
-    align-items: center;
-  }
 `;
