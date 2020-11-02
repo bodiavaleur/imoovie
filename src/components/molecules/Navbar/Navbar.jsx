541412160b7105c4fdc31f471b956a40e6ecb0cf
Bodia Valeur <bodiavaleur@Bodias-MacBook-Air.local>
2020-11-02 03:48:35 +0200

    Made navbar different for logged-in user and not logged-in

--- src/components/molecules/Navbar/Navbar.jsx
+++ src/components/molecules/Navbar/Navbar.jsx
@@ -1,15 +1,31 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { IconLink } from "../../atoms";
 import { NavbarUI } from "./NavbarUI";
-import { FaClock, FaSearch, FaStar, FaUserAltSlash } from "react-icons/fa";
+import {
+  FaClock,
+  FaSearch,
+  FaStar,
+  FaUserAlt,
+  FaUserAltSlash,
+} from "react-icons/fa";
 
 export function Navbar() {
+  const { user } = useSelector((state) => ({ user: state.user }));
   return (
     <NavbarUI>
       <IconLink to='/search' icon={<FaSearch />} label='Search' />
-      <IconLink to='/watchlist' icon={<FaClock />} label='Watchlist' />
-      <IconLink to='/favorites' icon={<FaStar />} label='Favorites' />
-      <IconLink to='/signout' icon={<FaUserAltSlash />} label='Signout' />
+      {user ? (
+        <>
+          <IconLink to='/watchlist' icon={<FaClock />} label='Watchlist' />
+          <IconLink to='/favorites' icon={<FaStar />} label='Favorites' />
+          <IconLink to='/signout' icon={<FaUserAltSlash />} label='Signout' />
+        </>
+      ) : (
+        <>
+          <IconLink to='/signin' icon={<FaUserAlt />} label='Sign in' />
+        </>
+      )}
     </NavbarUI>
   );
 }
