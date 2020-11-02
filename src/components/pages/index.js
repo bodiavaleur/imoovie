74f81040d77d98b5c70ce1100943c263dcb0829d
Bodia Valeur <bodiavaleur@Bodias-MacBook-Air.local>
2020-11-02 03:49:36 +0200

    Added watchlist and favorites pages

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -3,3 +3,5 @@ export { SignPage } from "./SignPage/SignPage";
 export { ContentDetails } from "./ContentDetails/ContentDetails";
 export { TopicContent } from "./TopicContent/TopicContent";
 export { SearchPage } from "./SearchPage/SearchPage";
+export { FavoritesPage } from "./FavoritesPage/FavoritesPage";
+export { WatchlistPage } from "./WatchlistPage/WatchlistPage";
