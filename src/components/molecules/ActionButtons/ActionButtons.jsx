37b904c9ae159ab2f7f6896a59558ce482932a50
Bodia Valeur <bodiavaleur@Bodias-MacBook-Air.local>
2020-11-02 03:47:50 +0200

    Made buttons to add to favorite or watchlist

--- src/components/molecules/ActionButtons/ActionButtons.jsx
+++ src/components/molecules/ActionButtons/ActionButtons.jsx
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { FaClock, FaStar } from "react-icons/fa";
 import { Button } from "../../atoms";
 import { ActionButtonsUI } from "./ActionButtonsUI";
+import { addUserContent } from "../../../firebase/firebaseApi";
+import { AiOutlineClockCircle, AiOutlineStar } from "react-icons/ai";
 
-export function ActionButtons() {
+export function ActionButtons({ content, type }) {
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [isInWatchlist, setIsInWatchlist] = useState(false);
+  const { user } = useSelector((state) => ({ user: state.user }));
+
+  const clickFavorite = () => {
+    addUserContent("favorites", user.uid, content, type, isFavorite);
+    setIsFavorite(!isFavorite);
+  };
+
+  const clickWatchlist = () => {
+    addUserContent("watchlist", user.uid, content, type, isInWatchlist);
+    setIsInWatchlist(!isInWatchlist);
+  };
+
   return (
     <ActionButtonsUI>
-      <Button>
-        <FaStar /> Favorite
+      <Button onClick={clickFavorite}>
+        {isFavorite ? (
+          <>
+            <AiOutlineStar /> Unfavorite
+          </>
+        ) : (
+          <>
+            <FaStar /> Favorite
+          </>
+        )}
       </Button>
-      <Button>
-        <FaClock /> Watchlist
+      <Button onClick={clickWatchlist}>
+        {isInWatchlist ? (
+          <>
+            <AiOutlineClockCircle /> Watched
+          </>
+        ) : (
+          <>
+            <FaClock /> Watchlist
+          </>
+        )}
       </Button>
     </ActionButtonsUI>
   );
