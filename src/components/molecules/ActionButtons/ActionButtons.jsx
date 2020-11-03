391e661465ef0b723b335e3aaf41b25df96e108e
Bodia Valeur <bodiavaleur@Bodias-MacBook-Air.local>
2020-11-03 18:20:31 +0200

    Made action buttons to work properly

--- src/components/molecules/ActionButtons/ActionButtons.jsx
+++ src/components/molecules/ActionButtons/ActionButtons.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { FaClock, FaStar } from "react-icons/fa";
 import { Button } from "../../atoms";
@@ -6,7 +6,7 @@ import { ActionButtonsUI } from "./ActionButtonsUI";
 import { addUserContent } from "../../../firebase/firebaseApi";
 import { AiOutlineClockCircle, AiOutlineStar } from "react-icons/ai";
 
-export function ActionButtons({ content, type }) {
+export function ActionButtons({ content, type, userData }) {
   const [isFavorite, setIsFavorite] = useState(false);
   const [isInWatchlist, setIsInWatchlist] = useState(false);
   const { user } = useSelector((state) => ({ user: state.user }));
@@ -20,6 +20,12 @@ export function ActionButtons({ content, type }) {
     addUserContent("watchlist", user.uid, content, type, isInWatchlist);
     setIsInWatchlist(!isInWatchlist);
   };
+
+  useEffect(() => {
+    const { favorites, watchlist } = userData;
+    favorites.find((fav) => fav.id === content.id) && setIsFavorite(true);
+    watchlist.find((wl) => wl.id === content.id) && setIsInWatchlist(true);
+  }, [content.id, userData]);
 
   return (
     <ActionButtonsUI>
--- src/components/pages/ContentDetails/ContentDetails.jsx
+++ src/components/pages/ContentDetails/ContentDetails.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { findById } from "../../../api";
+import { getUserData } from "../../../firebase/firebaseApi";
 import { Poster } from "../../atoms";
 import { ActionButtons, Details } from "../../molecules";
 import { DefaultTemplate } from "../../templates/";
@@ -7,13 +9,17 @@ import { ContentDetailsHeadUI, ContentDetailsUI } from "./ContentDetailsUI";
 
 export function ContentDetails(props) {
   const [details, setDetails] = useState(null);
+  const [userData, setUserData] = useState(null);
   const contentType = props.match.params.type;
   const contentId = props.match.params.id;
   const backdropBaseUrl = "http://image.tmdb.org/t/p/w500/";
+  const { user } = useSelector((state) => ({ user: state.user }));
 
   useEffect(() => {
     findById(contentType, contentId).then((data) => setDetails(data));
-  }, [contentType, contentId]);
+    // if user is logged in - get his favorites and watchlist
+    user && getUserData(user.uid).then((data) => setUserData(data));
+  }, [contentType, contentId, user]);
 
   return (
     <DefaultTemplate>
@@ -25,7 +31,14 @@ export function ContentDetails(props) {
             >
               <Poster img={details.poster_path} size='big' />
             </ContentDetailsHeadUI>
-            <ActionButtons content={details} type={contentType} />
+            {user && userData && (
+              <ActionButtons
+                content={details}
+                type={contentType}
+                userData={userData}
+              />
+            )}
+
             <Details details={details} />
           </>
         )}
